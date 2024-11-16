83fd9fd886c9982daedccdd9c6d1a07d6624ad80
Paul <ppagliughi@yahoo.com>
2024-11-16 08:41:48 -0600

    First commit

---
+++ netlify/functions/pets.js
@@ -0,0 +1,18 @@
+const handler = async () => {
+
+
+
+
+  return {
+
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: "paul".toLocaleUpperCase
+
+
+  }
+
+}
+
+module.exports = { handler }
+
